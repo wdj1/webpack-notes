c42864ce5cec74642c186694f3c3450504d14df4
wdj1 <wdj1@foxmail.com>
2018-06-19 21:09:02 +0800

    lesson06-html-webpack-plugin

---
+++ 06/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer:{
+    //本地服务路径
+    contentBase:"./dist",
+    //实时刷新
+    inline:true
+  },
+  module: {
+    rules: [
+      { test:/\.css$/, use: ['style-loader', 'css-loader'] },
+      { test:/\.(jpg|png|jpeg)$/, use: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        //去除引号
+        removeAttributeQuotes: true,
+        //去除注释
+        removeComments: true,
+        //去除空属性
+        removeEmptyAttributes: true,
+        //去除空格
+        collapseWhitespace:true
+      }
+    })
+  ]
+};
